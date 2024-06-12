57d287722f6027297044339b18b00d5ad4255d96
Gia Thinh Nguyen <gthinh.nguyen.dev@gmail.com>
2024-06-11 20:42:45 -0400

    fix missing fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,44 @@
 import { SplashScreen as ExpoSplashScreen, Stack } from 'expo-router';
 
 import { StatusBar } from 'expo-status-bar';
-import { useFonts } from 'expo-font';
 import SplashScreen from '../components/SplashScreen';
 import { ThemeProvider } from '@react-navigation/native';
 import { LightTheme, DarkTheme } from '@/styles/navigationTheme';
 import { useEffect, useState } from 'react';
 import { useColorScheme } from 'react-native';
-import { I18nextProvider } from 'react-i18next';
+import { I18nextProvider, useTranslation } from 'react-i18next';
 import i18nConfig from '@/locales/i18n';
+import {
+  useFonts,
+  Lato_100Thin,
+  Lato_100Thin_Italic,
+  Lato_300Light,
+  Lato_300Light_Italic,
+  Lato_400Regular,
+  Lato_400Regular_Italic,
+  Lato_700Bold,
+  Lato_700Bold_Italic,
+  Lato_900Black,
+  Lato_900Black_Italic
+} from '@expo-google-fonts/lato';
 
 ExpoSplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function AppLayout() {
   const currentTheme = useColorScheme();
+  const { t } = useTranslation();
   const [isAppReady, setIsAppReady] = useState(false);
   const [loaded] = useFonts({
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf')
+    Lato_100Thin,
+    Lato_100Thin_Italic,
+    Lato_300Light,
+    Lato_300Light_Italic,
+    Lato_400Regular,
+    Lato_400Regular_Italic,
+    Lato_700Bold,
+    Lato_700Bold_Italic,
+    Lato_900Black,
+    Lato_900Black_Italic
   });
 
   useEffect(() => {
@@ -33,10 +54,14 @@ export default function AppLayout() {
     <I18nextProvider i18n={i18nConfig}>
       <ThemeProvider value={currentTheme === 'dark' ? DarkTheme : LightTheme}>
         {isAppReady ? (
-          <Stack>
+          <Stack initialRouteName="(tabs)">
             <Stack.Screen
               name="(tabs)"
-              options={{ headerShown: false }}
+              options={{ title: t('screen:home'), headerShown: false }}
+            />
+            <Stack.Screen
+              name="brew"
+              options={{ title: t('screen:home'), headerShown: false }}
             />
           </Stack>
         ) : (
